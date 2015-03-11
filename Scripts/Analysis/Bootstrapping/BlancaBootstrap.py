3eced7cf036822c7db1c9b251f9b3ad348802f1e
ljchang <lukejchang@gmail.com>
2015-03-10 18:42:45 -0600

    fixed coding line

--- Scripts/Analysis/Bootstrapping/BlancaBootstrap.py
+++ Scripts/Analysis/Bootstrapping/BlancaBootstrap.py
@@ -1,6 +1,5 @@
-#!/projects/luch0518/software/anaconda/bin/python
-
 # coding: utf-8 
+#!/projects/luch0518/software/anaconda/bin/python
 
 # This script divides up 5000 bootstrap samples into 20 jobs and automatically submits them to the blanca cluster.  It creates a shell script with an embedded matlab template file separately for each cluster job.  
 # Requires that the matlab .mat file load a fmri_data object with the .Y filled in and named 'data' 
