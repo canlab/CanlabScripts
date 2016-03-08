df835e52a5d098a63669b8a1ebd2b68c8c64ec98
Dan Weflen <danweflen@gmail.com>
2016-03-08 00:43:51 -0700

    This adds the path changing scripts to CanlabScripts.

---
+++ Scripts/Environment/path_12to8.m
@@ -0,0 +1,27 @@
+% This script changes the matlabpath so that it includes spm8
+% but not spm12. It assumes that both the spm8 and spm12 directories
+% are contained within the same directory. 
+
+m_path=strsplit(path(),':');
+spm12path_map= strfind(m_path,'spm12');
+pathlength=length(m_path);
+
+%Finding the spm12 directory in the current matlab path
+for i=1:pathlength
+  if any(spm12path_map{i});
+    spm12path=m_path{i};
+    break;
+  end
+end
+
+orig_dir=pwd();
+cd(spm12path);
+% This assumes that spm8 and spm12 have the same parent directory.
+cd('../spm8'); 
+spm8path=pwd();
+cd(orig_dir);
+
+rmpath(spm12path);
+addpath(spm8path);
+savepath;%Permanently saves the new path.
+
---
+++ Scripts/Environment/path_8to12.m
@@ -0,0 +1,27 @@
+% This script changes the matlabpath so that it includes spm12
+% but not spm8. It assumes that both the spm8 and spm12 directories
+% are contained within the same directory.
+
+m_path=strsplit(path(),':');
+spm8path_map= strfind(m_path,'spm8');
+pathlength=length(m_path);
+
+%This finds the spm8 path in the current matlab path
+for i=1:pathlength
+  if any(spm8path_map{i});
+    spm8path=m_path{i};
+    break;
+  end
+end
+
+%Finding spm12
+orig_dir=pwd(); %We'll need this later
+cd(spm8path);
+cd('../spm12'); % Assumes spm12 is in the same parent directory.
+spm12path=pwd();
+cd(orig_dir);
+
+rmpath(spm8path);
+addpath(spm12path);
+savepath;
+
