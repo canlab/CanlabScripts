5c443902425f87433911b196c273c429017246c9
Dan Weflen <danweflen@gmail.com>
2016-06-06 16:51:56 -0600

    Basic skeleton of "canlabulator".
    Canlabulator aims to organize messy directories into the canlab
    directory structure.

---
+++ Scripts/Organization/canlabulator.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+#This is the "canlabulator", a program for rearranging directories into the canlab data format.
+
+#Right now it's pretty unstable, so make sure to use the "shadow" program to make a shadow of the directory
+#you want to canlabulate before running this script.
+
+import os
+import re
