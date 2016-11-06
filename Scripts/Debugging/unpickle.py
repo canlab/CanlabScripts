6933ed8b7c85940e276f4744d226d49f9fa35fc2
Daniel Weflen <danweflen@gmail.com>
2016-11-06 15:08:36 -0700

    Added unpickle.py.

---
+++ Scripts/Debugging/unpickle.py
@@ -0,0 +1,18 @@
+#!/work/ics/data/projects/wagerlab/Resources/anaconda2/bin/python
+
+####################################################################
+# This program is designed to display the information contained in #
+# the .pklz files created by python scripts that use the nipy      #
+# framework. It unzips, unpickles, and displays the resulting      #
+# debugging information.                                           #
+####################################################################
+
+import pickle
+import gzip
+from sys import argv
+
+crashdump=pickle.load(gzip.open(argv[1]))
+
+print crashdump['node']
+for item in crashdump['traceback']:
+    print item
