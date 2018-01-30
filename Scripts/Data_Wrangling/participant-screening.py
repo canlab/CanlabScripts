5a628a133e2a6da1eb30b084d4649146d6cbf55d
Daniel Weflen <weflen@colorado.edu>
2018-01-30 13:02:03 -0700

    Add a bunch of date screening to participant_screening.py

--- Scripts/Data_Wrangling/participant-screening.py
+++ Scripts/Data_Wrangling/participant-screening.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-#Note: This requires that pandas, numpy, and scipy are installed, and runs python 2.
+#Note: This requires that pandas, numpy, and scipy are installed, and runs python 3.
 
 #Usage: ./participant_screening.py <CSV filename> <lines to take>
 
@@ -11,15 +11,34 @@ import numpy as np
 import scipy as sp
 import pandas as pd
 import sys, os
+import datetime as dt
+from math import floor
 
 #This is the list of column headers to output.
-column_out=["participant_info_complete","name1","email_address1","phone_number1","session_availability1","which_meds"]
+column_out=["participant_info_complete","name1","email_address1","phone_number1","session_availability1","which_meds","date_of_birth1"]
+
+# Get approximate age from date of birth. This is imprecise and fudges leap days,
+# but should never be more than a few days off.
+
+def age_from_date(strdob):
+    dateparts=strdob.split('/')
+    dateparts=[int(x) for x in dateparts]
+    dob=dt.date(month=dateparts[0], day=dateparts[1], year=dateparts[2]) # Date object
+    age=dob.today()-dob
+    return floor(age.days/365.25)
 
 #This is the function that defines the relevant lines to
 #print. It is a boolean function that takes one argument,
 #the dataframe to filter.
+
 def filter_function(df):
+    strdob=df['date_of_birth1']
+    age=age_from_date(strdob)
+    youngerThan70=age < 70 # Younger than 70
+    olderThan21=age > 21  # Older than 21
+    ageOK=youngerThan70 and olderThan21
     return  (df['consent1'] == 1 and
+             ageOK and
              df['authorization___1'] == 1 and
              df['consent_to_contact1'] == 1 and
              df['painstudies'] == 1 and
@@ -32,7 +51,11 @@ def filter_function(df):
              df['pain_amount1'] == 0 and
              (df['meds'] <= 1 or np.isnan(df['meds'])) and
              df['fmri_studies_consent'] == 1 and
-             df['study_screening1___6'] == 1 and
+             df['study_screening1___2'] == 0 and
+             df['study_screening1___3'] == 0 and
+             df['study_screening1___4'] == 0 and
+             df['study_screening1___5'] == 0 and
+             df['date_of_birth1']
              df['mri_screening1___16'] <= 1 and
              df['mri_screening1___19'] <= 1 and
              (df['welder_machinist'] == 0 or np.isnan(df['metal_eyes1'])) and
@@ -51,7 +74,8 @@ screening_dataframe=pd.read_csv(filename)
 if endlines:
     screening_dataframe=screening_dataframe[-last_lines:]
 filtered_dataframe=screening_dataframe[screening_dataframe.apply(filter_function, axis=1)]
+filtered_dataframe['date_of_birth1']=filtered_dataframe['date_of_birth1'].apply(age_from_date)
 output_dataframe=filtered_dataframe[column_out]
 
-print output_dataframe.to_csv(index=False)
+print (output_dataframe.to_csv(index=False))
 
