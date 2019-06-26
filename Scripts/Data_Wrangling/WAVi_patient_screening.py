8a0dcafa0ed52175419fdc6974df3de1fc1ee898
Clayton Schneider <claytonjschneider@gmail.com>
2019-06-25 19:30:49 -0600

    participant classifiers

--- Scripts/Data_Wrangling/WAVi_patient_screening.py
+++ Scripts/Data_Wrangling/WAVi_patient_screening.py
@@ -16,22 +16,40 @@ rd = pd.read_csv(sys.argv[1], index_col = 0)
 column_list = rd.columns.values.tolist()
 
 #CLEANING
-#remove bad rows
+#remove worthless columns
 rd.drop(rd.columns[[0,1,2,48]], axis=1, inplace=True)
 
-#exclude for age
-rd_filtered = rd.query(
-'age_in_range==1'
-)
+#filtering for all exclusions
+rd_filtered = rd[
+    (rd.age_in_range == 1) &
+    (((rd.backpain_length > 1) & (rd.backpain_frequency > 1) & (rd.backpain_intensity > 3)) |
+    ((rd.backpain_length == 0) & (rd.backpain_frequency == 1) & (rd.backpain_intensity == 0))) &
+    (rd.current_opioid_use == 2) &
+    (rd.pregnancy == 2) &
+    (rd.english_proficiency == 1) &
+    (rd.transportation == 1) &
+    (rd.sick == 2) &
+    (rd.immunosuppressant == 2) &
+    (rd.autoimmune == 2) &
+    (rd.have_you_ever_been_diagnos == 2) &
+    (rd.have_you_ever_been_diagnos2 == 2) &
+    (rd.in_the_past_year_have_you == 2) &
+    (rd.do_you_have_difficulty_con == 2) &
+    (rd.stroke_neurological_event == 2) &
+    (rd.do_you_use_intravenous_dru == 2) &
+    (rd.welder == 2) &
+    (rd.metaleye == 2) &
+    (rd.metalbody == 2) &
+    (rd.claustrophobic == 2) &
+    (rd.mri_safety == "20")
+]
 
-#create exclusion flags
+#remove worthless columns
+rd_filtered.drop(rd_filtered.columns[[0,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,35,36,38,39,40,41,42,43]], axis=1, inplace=True)
 
-#filter for MRI
 #assign patient/control labels
 
-#filter for exclusion
-#patient
-#control
+#filter for patient/control specifics
 
 #print results to new csv sheet
 #name
@@ -41,4 +59,4 @@ rd_filtered = rd.query(
 #email
 #phone
 rd_filtered.to_csv('~/Documents/WAVi/rdprocessed.csv')
-print(rd)
+print(rd_filtered)
