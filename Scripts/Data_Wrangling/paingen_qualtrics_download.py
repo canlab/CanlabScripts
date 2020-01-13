146c56eb2cd87032a517e088a05e5064f656cd3e
Clayton <claytonjschneider@gmail.com>
2020-01-13 11:25:09 -0700

    qualtrics survey fetch script

---
+++ Scripts/Data_Wrangling/paingen_qualtrics_download.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+
+import requests
+import zipfile
+import json
+import io, os
+import sys
+import re 
+
+# from https://api.qualtrics.com/docs/getting-survey-responses-via-the-new-export-apis
+
+def exportSurvey(apiToken, surveyId, dataCenter, fileFormat):
+
+    surveyId = surveyId
+    fileFormat = fileFormat
+    dataCenter = dataCenter
+
+    # setting static parameters
+    requestCheckProgress = 0.0
+    progressStatus = "inProgress"
+    baseUrl = "https://{0}.qualtrics.com/API/v3/surveys/{1}/export-responses/".format(dataCenter, surveyId)
+    headers = {
+    "content-type": "application/json",
+    "x-api-token": apiToken,
+    }
+
+    # step 1: creating data export
+    downloadRequestUrl = baseUrl
+    downloadRequestPayload = '{"format":"' + fileFormat + '"}'
+    downloadRequestResponse = requests.request("POST", downloadRequestUrl, data=downloadRequestPayload, headers=headers)
+    progressId = downloadRequestResponse.json()["result"]["progressId"]
+    print(downloadRequestResponse.text)
+
+    # step 2: checking on data export progress and waiting until export is ready
+    while progressStatus != "complete" and progressStatus != "failed":
+        print ("progressStatus=", progressStatus)
+        requestCheckUrl = baseUrl + progressId
+        requestCheckResponse = requests.request("GET", requestCheckUrl, headers=headers)
+        requestCheckProgress = requestCheckResponse.json()["result"]["percentComplete"]
+        print("Download is " + str(requestCheckProgress) + " complete")
+        progressStatus = requestCheckResponse.json()["result"]["status"]
+
+    # step 2.1: check for error
+    if progressStatus is "failed":
+        raise Exception("export failed")
+
+    fileId = requestCheckResponse.json()["result"]["fileId"]
+
+    # step 3: downloading file
+    requestDownloadUrl = baseUrl + fileId + '/file'
+    requestDownload = requests.request("GET", requestDownloadUrl, headers=headers, stream=True)
+
+    # step 4: unzipping the file
+    zipfile.ZipFile(io.BytesIO(requestDownload.content)).extractall("MyQualtricsDownload")
+    print('Complete')
+
+def main():
+
+    try:
+        apiToken = os.environ['Q_API_TOKEN']
+        dataCenter = os.environ['Q_DATA_CENTER']
+    except KeyError:
+        print("set environment variables Q_API_TOKEN and Q_DATA_CENTER")
+        sys.exit(2)
+
+    try:
+        surveyId = sys.argv[1]
+        fileFormat = sys.argv[2]
+    except IndexError:
+        print ("usage: surveyId fileFormat")
+        sys.exit(2)
+
+    if fileFormat not in ["csv", "tsv", "spss"]:
+        print ('fileFormat must be either csv, tsv, or spss')
+        sys.exit(2)
+
+    r = re.compile('^SV_.*')
+    m = r.match(surveyId)
+    if not m:
+        print ("Survey Id must match ^SV_.*")
+        sys.exit(2)
+
+    exportSurvey(apiToken, surveyId, dataCenter, fileFormat)
+
+if __name__== "__main__":
+    main()
